521ba9c074f728899a98f825e8ea503dfe728e7a
pwnyboi <112465699+pwnyboi@users.noreply.github.com>
2022-08-30 01:39:25 -0600

    Update Network_Tutor.py

--- Network_Tutor.py
+++ Network_Tutor.py
@@ -1,5 +1,14 @@
+#This is a netowrking fundamentals helpful tool!
 #Purpose of this tool is:
 #1. Train Fundamentals of Networking Concepts
 #2. Test those concepts
 #3. Provide feedback on learning progress
 
+
+
+from pyfiglet import Figlet
+from termcolor import colored, cprint
+import time
+
+network_tutor_banner = pyfiglet.figlet_format("NetworkTutor")
+print(colored(network_tutor_banner,'green'))
